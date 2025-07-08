0fc0636f5034ae759434601f4bfa017aa6dce19c
Mayuri0602 <mayurimaheshwari760@gmail.com>
2025-07-08 14:47:42 +0530

    Bitwise Operators

---
+++ 08_July.py
@@ -0,0 +1,37 @@
+# Shorthand Operators
+# +=
+# -=
+# *=
+# /=
+
+
+# Bitwise Operators:-
+# | -> or
+# & -> and
+# ^ -> XOR
+# ~ -> not
+# >> -> right shift
+# << -> left shift
+
+a = 35
+print(bin(a))
+
+# 0r
+a = 21
+b = 17
+print(bin(a))
+print(bin(b))
+print(a|b)
+
+# and
+c = 23
+d = 13
+print(bin(c))
+print(bin(d))
+print(c&d)
+
+# not
+print(~7)
+
+# xor
+print(21^35)
