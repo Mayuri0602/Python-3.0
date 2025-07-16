263681188a9fd7f998d550d4ec29ac766aba5da5
Mayuri0602 <mayurimaheshwari760@gmail.com>
2025-07-16 23:53:03 +0530

    While loop basic questions

---
+++ 15_July.py
@@ -0,0 +1,101 @@
+# i = 10
+# while i >= 1:
+#     print(i)
+#     i-=1
+
+# i = 30
+# while (i >= 21):
+#   if (i%2==0):
+#     print(i)
+#   i-=1    
+
+# i = 1
+# sum = 0
+# while (i <= 10):
+#     sum += i
+#     i += 1
+# print(sum)
+
+# n = 1
+# while(n<=5):
+#     print(n*n)    
+#     n+=1
+
+# i=21
+# while i>=15:
+#     print(i**0.5)
+#     i-=1
+
+# a = 1
+# while a<6:
+#     if a==3:
+#         break
+#     a+=1
+
+# n=int(input("Enter a number:"))
+# i=1
+# while(n>=i):
+#        print(i)
+#        i+=1
+
+
+# n=int(input("Enter a number:"))
+# while(n>=1):
+#      print(n) 
+#      n-=1    
+
+# a = 97
+# while a <= 122:
+#     print(chr(a))
+#     a+=1
+
+# n = 1
+# while n <= 100:
+#     if(n%2==0):
+#       print(n)        
+#     n+=1 
+
+# n=1
+# sum_odd=0
+# while (n <= 100):
+#       if(n%2!=0):
+#          sum_odd+=n
+#       n+=1 
+# print(sum_odd)    
+
+# n = int(input("Enter a number: "))
+# count=0
+# while(n>0):
+#      n=n//10
+#      count+=1
+# print(count)  
+
+# n=int(input("Enter a number:"))
+# sum=0
+# while(n>0):
+#     rem=n%10
+#     sum+=rem
+#     n=n//10
+# print(sum)    
+
+# n=int(input("Enter a number:"))
+# last_digit=n%10
+# while(n>=10):
+#     n=n//10
+# print("First_digit=",n)
+# print("last digit=",last_digit)    
+
+# n=int(input("Enter a number:"))
+# last_digit=n%10
+# while(n>=10):
+#     n=n//10
+#     sum=last_digit+n
+# print("Sum of first and last digit =",sum)    
+
+
+
+# a=191
+# first_digit=a%10
+# while(a>0):
+#     last_digit=a
+# print(first_digit,last_digit)    
