64444c9f099273690bbcadf05fd4240447ed6753
Mayuri0602 <mayurimaheshwari760@gmail.com>
2025-08-25 15:28:54 +0530

    Basic terms and concept of functions

--- Functions_01.py
+++ Functions_01.py
@@ -1,4 +1,4 @@
-# Functionn is a reusable block of code that performs a specific task.
+# Functionn is a reusable block of code that performs a specific task and makes the code more readable.
 
 # Parameters are the variables passing at the time of defining a function.
 
@@ -52,20 +52,24 @@ while True:
          break
 
 
+
 # Function with parameter and without return
 def add(a,b):
     print(a+b)
 add(10,90)
+
 
 # Function with parameter and return
 def sub(a,b):
     return(a-b)
 print(sub(100,90))
 
+
 # Function without parameter and return
 def mul():
     return (10*10)
 print(mul())
+
 
 # Function without parameter and without return
 def div():
@@ -74,8 +78,23 @@ div()
 
 
 
-# Return is a keyword used to close the function body. (any variable or expression can be used)
-# If we want to display the return value then we have to call the function with print 
+# Return is a keyword used to end the execution of the function call and returns the value of the expression. (any variable or expression can be used)
+# If we want to display the return value then we have to call the function with print() function.
+# The statements after the return statements are not executed. If the return statement is without any expression, then the special value "None" is returned.
+
+def cube(a):
+    return (a**3)
+
+cube(3)              # output show nahi hoga
+print(cube(5))       # 125
+
 
 
 # Default value is the value we stored in parameter so that if we don't pass the value in argument, it will work as an argument.
+
+def greet(name="admin"):
+    return ("Hello, " + name + " !")
+
+print(greet())
+print(greet("user"))
+
